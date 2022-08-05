3cfb9572c39fce9e300bb107777c45e404137ffd
Gino Boca <gnoboca@gmail.com>
2022-08-05 12:59:48 -0300

    update

--- index.js
+++ index.js
@@ -14,13 +14,43 @@ const swiper = new Swiper('.swiper', {
 
 })
 
+let button = document.getElementById('button');
+
+button.addEventListener('click', function(){
+	if(button.classList.contains('button')){
+		button.classList.remove('button')
+		button.classList.add('button-heart-active')
+	} else{
+		button.classList.remove('button-heart-active')
+		button.classList.add('button')
+	}
+})
 
 
-const getImages = async() =>{
-	const response = await fetch('https://picsum.photos/v2/list?page=2&limit=100')
-    const data = await response.json()
-	const img = document.getElementById('img').src = data.url;
+/* problem with the location of the fetching images into the html*/
+
+const requestUrl = 'https://picsum.photos/v2/list?page=2&limit=100';
+
+/*
+const getImages = async () =>{
+	const response = await fetch(requestUrl)
+	const data = await response.json()
+	const arrayImg = data
+	const newArray = arrayImg.slice(1,4)
+	
+	newArray.map((e) =>{
+		const url = e.url
+		
+		const image = `<img src = ${url} />`
+		document.getElementsByClassName('image__wrapper').innerHTML += image
+	})
 }
+*/
+
+
+
+
+
 
 
 
